d8002142c7eda01e224afa7daa5eff89a3e10294
zhangbaoshan2003 <zhangbaoshan2003@hotmail.com>
2017-03-09 22:44:15 +0800

    use timeout to process layout issue

--- Galaxy/Scripts/Product/productDataDetail.js
+++ Galaxy/Scripts/Product/productDataDetail.js
@@ -1,19 +1,22 @@
-﻿$(function () {
+﻿var Global_oTable;
+$('#btnTabOne').click(function () {
+    setTimeout(function () { Global_oTable.draw(); }, 1);
+});
+$(function () {
     //initialize data table 
-    $('#prodcutDetailTable').DataTable({
+    Global_oTable = $('#prodcutDetailTable').DataTable({
         "paging": true,
         "lengthChange": true,
         "searching": true,
         "ordering": true,
         "info": false,
-        "autoWidth": false,
+        "autoWidth": true,
         "scrollX": true,
         "sScrollX": "100%",
-        "sScrollXInner": "110%",
-        "bScrollCollapse": true
+        "sScrollXInner": "110%"
     });
 
     //fix display issue for a datatable in a tab div initially
-    $('div.dataTables_scrollHeadInner').css('width', '110%');
-    $('div.dataTables_scrollHeadInner table').css('width', '110%');
+    //$('div.dataTables_scrollHeadInner').css('width', '110%');
+    //$('div.dataTables_scrollHeadInner table').css('width', '110%');
 });
